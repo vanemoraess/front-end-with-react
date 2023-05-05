679fd46e6e7f55bd60546370d6a9c2712bbab0ae
vanemoraess <vanessamoraes@ufrj.br>
2023-05-05 00:31:06 -0300

    endereço

--- front-programaria-react-main/src/components/Content.jsx
+++ front-programaria-react-main/src/components/Content.jsx
@@ -14,7 +14,7 @@ export function Content() {
   const [citacao, setCitacao] = useState('')
   const [imagem, setImagem] = useState('')
   const [success, setSuccess] = useState(false)
-  const baseURL = 'https://back-end-6c7c.onrender.com/mulheres'
+  const baseURL = 'https://express-get-practice.onrender.com/women'
 
   useEffect(() => {
     async function getData() {
